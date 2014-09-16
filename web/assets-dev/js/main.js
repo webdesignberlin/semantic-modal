e4c817d2c60c5b2d7bd8cbb6f828ad01f32e06c2
Michael Gerstmann <mg@bytepark.de>
2014-09-16 11:20:18 +0200

    config js files for semantic modal

---
+++ web/assets-dev/js/main.js
@@ -0,0 +1,9 @@
+;(function( $, window, document, undefined ) {
+    // well, it's pretty strict...
+    'use strict';
+
+    console.debug('Hallo Welt.');
+
+    console.debug( x );
+
+})(jQuery,window,document);
