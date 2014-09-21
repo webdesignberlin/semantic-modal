3a3f0455c85b8bcf52ecd5acd92e1326ab92bef9
webdesignberlin <reg@seiler-gerstmann.de>
2014-09-21 13:08:35 +0200

    Bugfixes, add role attributes
    fix arialabels, change semantic order, add role attributes, cleanup

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <?php include 'inc/_head.php'; ?>
-<header>
+<header role="banner">
     <div class="container">
         <h1>semantic modal</h1>
     </div>
 </header>
-<main>
+<main role="main">
     <article class="container">
 
         <h2>Demo Content</h2>
@@ -12,14 +12,10 @@
 
         <button type="button" class="modal-open" data-modal-target="modal-dialog">Modal öffnen</button>
 
-
-        <section class="modal modal-dialog" id="modal-dialog"
-             aria-hidden="true" role="dialog"
-             aria-labelledby="modal__title">
-
+        <section class="modal modal-dialog" id="modal-dialog" role="dialog" aria-hidden="true" aria-labelledby="modal-title">
             <div class="modal__content modal-holder" role="document">
 
-                <h1 class="modal__content__title">Modal Title</h1>
+                <h1 class="modal__content__title" id="modal-title">Modal Title</h1>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
 
                 <button class="btn-modal__close modal-close" data-modal-target="modal-dialog" type="button" aria-label="close">
@@ -30,22 +26,13 @@
         </section>
 
 
-
-
-
-
         <button type="button" class="modal-open" data-modal-target="modal-dialog-2">Modal 2 öffnen</button>
-
-
-        <section class="modal modal-dialog" id="modal-dialog-2"
-                 aria-hidden="true" role="dialog"
-                 aria-labelledby="modal__title">
-
+        
+        <section class="modal modal-dialog" id="modal-dialog-2" aria-hidden="true" role="dialog" aria-labelledby="modal-title-2">
             <div class="modal__content modal-holder" role="document">
 
-                <h1 class="modal__content__title">Modal Title 2</h1>
+                <h1 class="modal__content__title" id_="modal-title-2">Modal Title 2</h1>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
                 <button class="btn-modal__close modal-close" data-modal-target="modal-dialog-2" type="button" aria-label="close">
                     &times; Modal schließen
                 </button>
@@ -54,8 +41,8 @@
         </section>
 
     </article>
-    <footer class="container">
-        <p><a href="http://micha.seiler-gerstmann.de">Webdesign Berlin, Michael Seiler-Gerstmann</a> | <a href="https://github.com/webdesignberlin/semantic-modal">Find Semantic Modal on Github</a></p>
-    </footer>
 </main>
+<footer class="container" role="contentinfo">
+    <p><a href="http://micha.seiler-gerstmann.de">Webdesign Berlin, Michael Seiler-Gerstmann</a> | <a href="https://github.com/webdesignberlin/semantic-modal">Find Semantic Modal on Github</a></p>
+</footer>
 <?php include 'inc/_footer.php'; ?>
